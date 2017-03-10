c2bba83d0d1a9f68659e06e49b7da6894148be28
rusel95 <ruslanpopesku95@gmail.com>
2017-03-10 16:30:37 +0200

    1) MessageController 2) NewMessageViewController 3) UserClass All users from Firebase in app

---
+++ Chatster/MessagesController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Chatster
+//
+//  Created by Admin on 08.03.17.
+//  Copyright © 2017 rusel95. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MessagesController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "new_message_icon"), style: .plain, target: self, action: #selector(handleNewMessage))
+        
+        checkIfUserIsLoggedIn()
+    }
+    
+    func checkIfUserIsLoggedIn() {
+        if FIRAuth.auth()?.currentUser?.uid == nil {
+            perform(#selector(handleLogout), with: nil, afterDelay: 0)
+        } else {
+            let uid = FIRAuth.auth()?.currentUser?.uid
+            FIRDatabase.database().reference().child("users").child(uid!).observe(.value, with: { (snapshot) in
+                
+                if let dictionary = snapshot.value as? [String: AnyObject] {
+                    self.navigationItem.title = dictionary["name"] as? String
+                }
+                
+            }, withCancel: nil)
+        }
+    }
+    
+    func handleNewMessage() {
+        let newMessageController = NewMessageViewController()
+        let navController = UINavigationController(rootViewController: newMessageController)
+        present(navController, animated: true, completion: nil)
+    }
+
+    func handleLogout() {
+        
+        do {
+            try FIRAuth.auth()?.signOut()
+        } catch let logoutError {
+            print(logoutError)
+        }
+        
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+
+}
+
